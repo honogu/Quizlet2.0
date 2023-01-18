cf5d7b1c9a65493be59a234073615e1a0d2363e5
Holger Nõgu <honogu@ttu.ee>
2023-01-19 01:16:49 +0200

    changes

---
+++ src/models/Messages.ts
@@ -0,0 +1,21 @@
+import { ref } from 'vue';
+
+export const correctMessages = ref<string[]>([
+    'Hea töö!',
+    'Tore, said aru!',
+    'Vinge!',
+    'Suurepärane!',
+    'See on sul käes!',
+    'Sa oled väga tubli!',
+    'Jätka samas vaimus!',
+    'You go girl!'
+]);
+
+export const wrongMessages = ref<string[]>([
+    'Ära muretse, sa alles õpid!',
+    'Mitte päris, sa alles õpid!',
+    'Ära muretse, õppimine on protsess!',
+    'Vaikselt tuleb!',
+    'Iz okay!',
+    'Pole hullu!'
+]);
