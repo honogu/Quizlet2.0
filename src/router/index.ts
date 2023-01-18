032d14a9da0837e3909d08b5aee3251a84f3b253
Holger Nõgu <honogu@ttu.ee>
2023-01-18 21:21:38 +0200

    initial commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,3 +21,13 @@ const router = createRouter({
 })
 
 export default router
+
+router.beforeEach((to, from, next) => {
+  // Redirect if fullPath begins with a hash (ignore hashes later in path)
+  if (to.fullPath.substr(0,2) === "/#") {
+      const path = to.fullPath.substr(2);
+      next(path);
+      return;
+  }
+  next();
+});
